0d1c691e30153df023fdbea04da641fa69d22401
Diya-CMD <diya.leda@hva.nl>
2024-10-03 14:21:10 +0200

    hamburger

--- README.md
+++ README.md
@@ -202,11 +202,11 @@ Color contrast
   ### Agenda voor meeting
   samen met je groepje opstellen
 
-  | student 1      | student 2          | student 3    | student 4        |
-  | ---            | ---                | ---          | ---              |
-  | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
-  | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
-  | ...            | ...                | ...          | ...              |
+  |     Diya        | Giulietta     | Keysha         | Thi          |
+  | ---             | ---           | ---            | ---          |
+  |   carousel      | *             | *              | Navigatie    |
+  | a over article? | *             | *              | img size     |
+  | ...             | ...           | ...            | ...          |
 
 
   ### Verslag van meeting
--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
-// JavaScript Document
-console.log("hi");
+
+// stap 1: zoek de menu-button op en sla die op in een variabele. De selectie is hetzelfde als in CSS en Java. > is voor een direct kind.
+const menuOpenButton= document.querySelector("header > button"); 
+const deNav= document.querySelector("header nav"); 
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+menuOpenButton.onclick =  burgerMenu; 
+
+
+// stap 3: voeg in de functie een class toe aan de nav. "alleen open" is add.
+function burgerMenu() {
+  deNav.classList.add("toonMenu");
+}
+
+/************************************/
+/* menu sluiten met de sluit button */
+/************************************/
+
+/* JOUW CODE HIER - stap 5 */
+
+// stap 1 - zoek sluiten button op
+const menuSluitButton= document.querySelector("nav button"); 
+
+// stap 2 - laat die button luisteren naar kliks
+menuSluitButton.onclick = sluitMenu; 
+
+// stap 3 - in de functie verwijder de class van de nav
+function sluitMenu(){
+   deNav.classList.remove("toonMenu");
+}
